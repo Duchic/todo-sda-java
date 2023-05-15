006197f27d2799142090042a33d9e1e8fe942abc
duchic1 <duchek.tomas@outlook.com>
2023-05-15 21:20:22 +0200

    Init project

---
+++ src/main/java/com/example/todosdajava/TodoSdaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.todosdajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSdaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoSdaJavaApplication.class, args);
+    }
+
+}
